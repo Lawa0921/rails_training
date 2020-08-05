d98fa825d92c019eabd08fc64dc264b68298908f
lawa0921 <bag571ivy3470@gmail>
2020-08-05 21:21:09 +0800

    改為 sku 欄位

--- app/models/good.rb
+++ app/models/good.rb
@@ -1,10 +1,8 @@
 class Good < ApplicationRecord
   has_many_attached :photos
   enum status: ["上架", "進貨中", "下架"]
-  enum size: ["小", "中", "大"]
-  enum color: ["黑", "白", "紅", "藍", "紫"]
+  validates :sku, presence: true, uniqueness: true
   validates :name, :price, :status, :discount, presence: true
-  validates :number, presence: true, uniqueness: true
   validates :inventory, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :discount, presence: true, numericality:  {greater_than_or_equal_to: 0, less_than_or_equal_to: 1 }
 end
