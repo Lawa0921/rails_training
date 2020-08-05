1a71e02818deb1960fe1bfd92ae908fc476f28f0
lawa0921 <bag571ivy3470@gmail>
2020-08-05 17:05:46 +0800

    可以刪除特定圖檔

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
   scope :admin do
     devise_for :admins
-    resources :goods
+    resources :goods do
+      member do
+        delete :remove_photo, path: "/:photo_id"
+      end
+    end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
