91312b27871246b65ece8bca62f70366e0d27f60
lawa0921 <bag571ivy3470@gmail>
2020-08-06 21:58:00 +0800

    enum modify

--- app/models/good.rb
+++ app/models/good.rb
@@ -1,6 +1,6 @@
 class Good < ApplicationRecord
   has_many_attached :photos
-  enum status: ["上架", "進貨中", "下架"]
+  enum status: ["put on the shelves", "in stock", "take down the shelves"]
   validates :sku, presence: true, uniqueness: true
   validates :name, :price, :status, :discount, presence: true
   validates :inventory, presence: true, numericality: { greater_than_or_equal_to: 0 }
